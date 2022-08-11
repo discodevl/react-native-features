de6da2c904fd382ef7cffa7222e127f5e43cd788
discodevl <donato.cutro3@gmail.com>
2022-08-11 15:32:35 +0200

    navigation implementation

--- components/places/PlacesList.js
+++ components/places/PlacesList.js
@@ -1,14 +1,31 @@
-import { FlatList } from "react-native";
+import { FlatList, StyleSheet, View, Text } from "react-native";
 import React from "react";
+import PlaceItem from "./PlaceItem";
 
 export default function PlacesList({ places }) {
-  function renderHandler() {}
+
+  if(!places || places.length === 0) {
+    return <View style={styles.fallbackContainer}>
+      <Text style={styles.fallbackText}>No places added</Text>
+    </View>
+  }
 
   return (
     <FlatList
       data={places}
       keyExtractor={(item) => item.id}
-      renderItem={renderHandler}
+      renderItem={({item}) => <PlaceItem place={item}/>}
     />
   );
 }
+
+const styles = StyleSheet.create({
+  fallbackContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  fallbackText: {
+    fontSize:16
+  }
+})
--- screens/AddPlace.js
+++ screens/AddPlace.js
@@ -1,10 +1,9 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import PlaceForm from '../components/places/PlaceForm'
 
 export default function AddPlace() {
   return (
-    <View>
-      <Text>AddPlace</Text>
-    </View>
+    <PlaceForm />
   )
 }
