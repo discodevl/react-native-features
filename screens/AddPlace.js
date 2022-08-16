174598cb21316de8d82fbcc32f355fadcf3c085c
discodevl <donato.cutro3@gmail.com>
2022-08-16 02:27:14 +0200

    inserto into db

--- screens/AddPlace.js
+++ screens/AddPlace.js
@@ -1,14 +1,13 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import PlaceForm from '../components/places/PlaceForm'
+import { View, Text } from "react-native";
+import React from "react";
+import PlaceForm from "../components/places/PlaceForm";
+import { insertPlace } from "../util/database";
 
-export default function AddPlace({navigation}) {
-  
-  function createPlace(place) {
-    navigation.navigate('AllPlaces', {place: place})
+export default function AddPlace({ navigation }) {
+  async function createPlace(place) {
+    await insertPlace(place);
+    navigation.navigate("AllPlaces", { place: place });
   }
 
-  return (
-    <PlaceForm onCreatePlace={createPlace}/>
-  )
+  return <PlaceForm onCreatePlace={createPlace} />;
 }
